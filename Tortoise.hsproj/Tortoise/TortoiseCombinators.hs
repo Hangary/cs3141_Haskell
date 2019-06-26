484a98f48e4f888cffb0bbda1cedea642a27ce30
Hangar <HangaryLiu@gmail.com>
2019-06-26 12:03:36 +1000

    fix

--- Tortoise.hsproj/Tortoise/TortoiseCombinators.hs
+++ Tortoise.hsproj/Tortoise/TortoiseCombinators.hs
@@ -16,33 +16,33 @@ import Tortoise
 -- getter
 getNextInstruction :: Instructions -> Instructions
 getNextInstruction instructions_1
-    = case instructions_1 of 
-          Move distance instructions_2 -> instructions_2
-          Turn angle instructions_2 -> instructions_2
-          SetStyle lineStyle instructions_2 -> instructions_2
-          SetColour colour instructions_2 -> instructions_2
-          PenDown instructions_2 -> instructions_2
-          PenUp instructions_2 -> instructions_2
+  = case instructions_1 of 
+      Move distance instructions_2 -> instructions_2
+      Turn angle instructions_2 -> instructions_2
+      SetStyle lineStyle instructions_2 -> instructions_2
+      SetColour colour instructions_2 -> instructions_2
+      PenDown instructions_2 -> instructions_2
+      PenUp instructions_2 -> instructions_2
 
 -- setter
 setNextInstruction :: Instructions -> Instructions -> Instructions
 setNextInstruction instructions_1 instructions_2 
-    = case instructions_1 of 
-          Move distance instructions -> Move distance instructions_2
-          Turn angle instructions -> Turn angle instructions_2
-          SetStyle lineStyle instructions -> SetStyle lineStyle instructions_2
-          SetColour colour instructions -> SetColour colour instructions_2
-          PenDown instructions -> PenDown instructions_2
-          PenUp instructions -> PenUp instructions_2
+  = case instructions_1 of 
+      Move distance instructions -> Move distance instructions_2
+      Turn angle instructions -> Turn angle instructions_2
+      SetStyle lineStyle instructions -> SetStyle lineStyle instructions_2
+      SetColour colour instructions -> SetColour colour instructions_2
+      PenDown instructions -> PenDown instructions_2
+      PenUp instructions -> PenUp instructions_2
 
 
 andThen :: Instructions -> Instructions -> Instructions
 andThen Stop instructions_2 = instructions_2
 andThen instructions_1 Stop = instructions_1
 andThen instructions_1 instructions_2 
-    = case (getNextInstruction instructions_1) of
-          Stop -> setNextInstruction instructions_1 instructions_2 
-          _    -> setNextInstruction instructions_1 $  (getNextInstruction instructions_1) `andThen` instructions_2
+  = case (getNextInstruction instructions_1) of
+      Stop -> setNextInstruction instructions_1 instructions_2 
+      _    -> setNextInstruction instructions_1 $  (getNextInstruction instructions_1) `andThen` instructions_2
 
 
 loop :: Int -> Instructions -> Instructions
@@ -76,7 +76,13 @@ retrace i = retrace_helper i Stop
 retrace_helper :: Instructions -> Instructions -> Instructions
 retrace_helper i last_i
   | i == Stop = last_i
-  | otherwise = retrace_helper (getNextInstruction i) $ setNextInstruction i last_i
+  | otherwise = retrace_helper (getNextInstruction i) $ setNextInstruction (reverseInstruction i) last_i
+  where 
+    reverseInstruction :: Instructions -> Instructions
+    reverseInstruction i = case i of 
+      Move distance instructions -> Move (-distance) instructions
+      Turn angle instructions -> Turn (-angle) instructions
+      _ -> i
 
 
 overlay :: [Instructions] -> Instructions
