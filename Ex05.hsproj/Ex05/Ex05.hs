912fee1f43608b051eb93b5f71bcb56880fbeb81
Hangar <HangaryLiu@gmail.com>
2019-07-18 18:33:40 +1000

    finished ex5

--- Ex05.hsproj/Ex05/Ex05.hs
+++ Ex05.hsproj/Ex05/Ex05.hs
@@ -8,11 +8,16 @@ import Control.Monad.State
 import System.Random
 import Test.QuickCheck
 
+
+-- task 1
+
 capitalise :: FilePath -> FilePath -> IO ()
 capitalise i o = do
             contents <- readFile i
             writeFile o (map toUpper contents)
 
+
+-- task 2
 
 sumFile :: IO ()
 sumFile = do
@@ -29,11 +34,18 @@ sumFile = do
           sumStrings s = show $ sum $ fmap read $ words s
        
 
+-- task 3
+
 data Player m = Player { guess :: m Int
                        , wrong :: Answer -> m ()
                        }
 data Answer = Lower | Higher
 
+-- x is the number we're trying to guess
+-- n is the number of guesses we get
+-- p is the player
+-- Returns whether or not the player managed to guess correctly 
+-- in the time limit
 guessingGame :: (Monad m) => Int -> Int -> Player m -> m Bool
 guessingGame x n p = go n
   where
@@ -44,6 +56,7 @@ guessingGame x n p = go n
        LT -> wrong p Lower  >> go (n-1)
        GT -> wrong p Higher >> go (n-1)
        EQ -> pure True
+
 
 human :: Player IO
 human = Player { guess = guess, wrong = wrong }
@@ -58,6 +71,7 @@ human = Player { guess = guess, wrong = wrong }
     wrong Lower  = putStrLn "Lower!"
     wrong Higher = putStrLn "Higher!"
 
+
 play :: IO ()
 play = do
   x <- randomRIO (1,100)
@@ -69,12 +83,23 @@ midpoint :: Int -> Int -> Int
 midpoint lo hi | lo <= hi  = lo + div (hi - lo) 2
                | otherwise = midpoint hi lo
 
-ai :: Player (State (Int,Int))
-ai = error "'ai' unimplemented"
 
+ai :: Player (State (Int,Int))
+ai = Player { guess = guess, wrong = wrong }
+  where
+    guess = do
+      (lo, hi) <- get
+      return $ midpoint lo hi
+    wrong Lower  = do 
+      (lo, hi) <- get
+      put (lo, (midpoint lo hi) - 1)
+    wrong Higher = do 
+      (lo, hi) <- get
+      put ((midpoint lo hi) + 1, hi)
 
 
 prop_basic (Positive n) = forAll (choose (1,n)) $ \x -> evalState (guessingGame x n ai) (1,n)
+
 
 prop_optimality (Positive n) = forAll (choose (1,n)) $ \x -> evalState (guessingGame x (bound n) ai) (1,n)
   where bound n = ceiling (logBase 2 (fromIntegral n)) + 1
